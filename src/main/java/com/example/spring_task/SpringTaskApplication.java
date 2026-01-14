eff008d943bb457e001a541386527f2d836a9f18
kerber <111589560+kkkerber@users.noreply.github.com>
2026-01-14 21:39:08 +0100

    initial commit

---
+++ src/main/java/com/example/spring_task/SpringTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaskApplication.class, args);
+	}
+
+}
